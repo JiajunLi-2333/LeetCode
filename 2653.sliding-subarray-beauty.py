bc916f24b95d87d724a54775b27da494ab91d717
JiajunLi <jl11895@nyu.edu>
2025-01-21 16:53:45 -0500

    2653 is here I left unfinished

---
+++ 2653.sliding-subarray-beauty.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=2653 lang=python3
+#
+# [2653] Sliding Subarray Beauty
+#
+
+# @lc code=start
+class Solution:
+    def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
+        
+# @lc code=end
+
