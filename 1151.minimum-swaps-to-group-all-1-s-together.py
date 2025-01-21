3639d1cf3404c3ba88f3c3deb4f19058bc0192b3
JiajunLi <jl11895@nyu.edu>
2025-01-21 11:15:45 -0500

    new problem solved

---
+++ 1151.minimum-swaps-to-group-all-1-s-together.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=1151 lang=python3
+#
+# [1151] Minimum Swaps to Group All 1's Together
+#
+
+# @lc code=start
+class Solution:
+    def minSwaps(self, data: list[int]) -> int:
+
+        #! This is step finds the number of ones in the data, which equals to the size of the window
+        num_ones = sum(data)
+        count = 0
+        ans = len(data)
+        #! This defines the edge case
+        if num_ones == 1 or num_ones == 0:
+            return 0
+        
+        for i in range(len(data)):
+            if data[i] == 1:
+                count += 1
+            if i < num_ones -1:
+                continue
+            ans = min(ans, num_ones - count)
+
+            if data[i - num_ones + 1] == 1:
+                count -= 1
+        return ans
+
+        
+# @lc code=end
+
