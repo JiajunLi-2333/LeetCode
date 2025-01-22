eb00034bcd3a8538fd8a837390933297e33251f5
JiajunLi <jl11895@nyu.edu>
2025-01-22 11:05:57 -0500

    star question 2653

--- 2653.sliding-subarray-beauty.py
+++ 2653.sliding-subarray-beauty.py
@@ -4,9 +4,39 @@
 # [2653] Sliding Subarray Beauty
 #
 
-# @lc code=start
+# @lc code=start]
 class Solution:
-    def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
-        
+    def getSubarrayBeauty(self, nums: list[int], k: int, x: int) -> list[int]:
+        #! This is the brutal force
+
+        #todo sub-problem:
+        #todo 1. In the subarray, find the number of negative integers
+        #todo 2. In the subarray, find the order of the numbers. where I need to fix it by iterating the subarray
+
+        ans = []        
+        #!check whether the element is negative or not
+        freq = [0] * 50
+
+        for i in range(len(nums)):
+            #Enter the window
+            if nums[i] < 0:
+                freq[50 + nums[i]] += 1   
+            if i < k - 1:
+                continue
+
+            #! Record the number of negative integers
+            count = 0
+            for j in range(50):
+                count += freq[j]
+                if count >=x:
+                    ans.append(j - 50)
+                    break
+            if count < x:
+                ans.append(0)
+            
+            #Exit the window
+            if nums[i - k + 1] < 0:
+                freq[50 + nums[i - k + 1]] -= 1
+        return ans
 # @lc code=end
 
