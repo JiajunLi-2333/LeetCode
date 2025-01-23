021132b1aff63108e75ff7f148b85aa5f782f572
JiajunLi <jl11895@nyu.edu>
2025-01-23 17:51:08 -0500

    Rolling Hash and Horner's Method

---
+++ 2156.find-substring-with-given-hash-value.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=2156 lang=python3
+#
+# [2156] Find Substring With Given Hash Value
+#
+
+# @lc code=start
+class Solution:
+    def subStrHash(self, s: str, power: int, modulo: int, k: int, hashValue: int) -> str:
+        
+# @lc code=end
+
