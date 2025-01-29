153cc77e5022ec9332f7bf5bf41d4549a227d302
JiajunLi <jl11895@nyu.edu>
2025-01-28 22:35:11 -0500

    Call it a day

---
+++ 1064.fixed-point.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1064 lang=python3
+#
+# [1064] Fixed Point
+#
+
+# @lc code=start
+class Solution:
+    def fixedPoint(self, arr: List[int]) -> int:
+        
+# @lc code=end
+
---
+++ 2555.maximize-win-from-two-segments.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=2555 lang=python3
+#
+# [2555] Maximize Win From Two Segments
+#
+
+# @lc code=start
+from collections import Counter
+class Solution:
+    def maximizeWin(self, prizePositions: list[int], k: int) -> int:
+        n = len(prizePositions)
+        if k * 2 + 1 >= prizePositions[-1] - prizePositions[0]:
+            return n
+        ans = left = 0
+        mx = [0] * (n + 1)
+        for right, p in enumerate(prizePositions):
+            while p - prizePositions[left] > k:
+                left += 1
+            ans = max(ans, mx[left] + right - left + 1)
+            mx[right + 1] = max(mx[right], right - left + 1)
+        return ans
+        
+# @lc code=end
+
