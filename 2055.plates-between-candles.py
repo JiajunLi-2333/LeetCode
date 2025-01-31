d7bbf055b8aa691fa7166b21aecee288507b72f8
JiajunLi <jl11895@nyu.edu>
2025-01-31 00:02:02 -0500

    Call it a day

---
+++ 2055.plates-between-candles.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=2055 lang=python3
+#
+# [2055] Plates Between Candles
+#
+
+# @lc code=start
+class Solution:
+    def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
+        
+# @lc code=end
+
--- 2438.range-product-queries-of-powers.py
+++ 2438.range-product-queries-of-powers.py
@@ -6,7 +6,32 @@
 
 # @lc code=start
 class Solution:
-    def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+    def productQueries(self, n: int, queries: list[list[int]]) -> list[int]:
+        MOD = 10**9 + 7
+        
+        # 1) Collect the powers of two from n's binary representation
+        #    In ascending order of bit positions (LSB -> MSB).
+        powers_array = []
+        bit_pos = 0
+        temp = n
+        while temp > 0:
+            if temp & 1:
+                powers_array.append(pow(2, bit_pos, MOD))
+            bit_pos += 1
+            temp >>= 1
+        
+        # 2) Answer each query by multiplying the relevant slice of powers_array
+        result = []
+        for left, right in queries:
+            product = 1
+            for i in range(left, right + 1):
+                product = (product * powers_array[i]) % MOD
+            result.append(product)
+        
+        return result
+        
+
+        
         
 # @lc code=end
 
