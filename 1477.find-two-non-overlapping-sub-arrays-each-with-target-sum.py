ebd482d500c279f9acb11abb226cd1b0a04e1488
JiajunLi <jl11895@nyu.edu>
2025-02-06 23:36:56 -0500

    call it a day

--- 1477.find-two-non-overlapping-sub-arrays-each-with-target-sum.py
+++ 1477.find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -5,8 +5,21 @@
 #
 
 # @lc code=start
+from collections import Counter
 class Solution:
-    def minSumOfLengths(self, arr: List[int], target: int) -> int:
+    def minSumOfLengths(self, arr: list[int], target: int) -> int:
+        hashmap = Counter({0:0})
+        preSum = 0
+        length_arr = list()
+
+        for i in range(len(arr)):
+            preSum += arr[i]
+            hashmap[preSum] = i + 1
+            if preSum - target in hashmap:
+                length_arr.append(hashmap[preSum] - hashmap[preSum - target])
+        length_arr.sort()
+        return -1 if len(length_arr) < 2 else sum(length_arr[:2])
+
         
-# @lc code=end
+# @lc code=end  
 
