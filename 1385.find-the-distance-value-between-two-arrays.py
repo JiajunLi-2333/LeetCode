de0d8f0f98e98f388261cb6db4cb1c9655e4e769
JiajunLi <jl11895@nyu.edu>
2025-02-19 20:59:49 -0500

    Call it a day

--- 1385.find-the-distance-value-between-two-arrays.py
+++ 1385.find-the-distance-value-between-two-arrays.py
@@ -5,8 +5,21 @@
 #
 
 # @lc code=start
+import bisect
 class Solution:
-    def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+    def findTheDistanceValue(self, arr1: list[int], arr2: list[int], d: int) -> int:
+        #todo use the binary search to find the distance value
+        arr2.sort()
+        ans = 0
+
+        for element in arr1:
+            lower_bound = element - d
+            upper_bound = element + d
+
+            if bisect.bisect_left(arr2, lower_bound) == bisect.bisect_right(arr2, upper_bound):
+                ans += 1
+        return ans
+
         
 # @lc code=end
 
