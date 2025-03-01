a6afca9f4c434e126c1ecb68971860ff2026a7ad
JiajunLi <jl11895@nyu.edu>
2025-03-01 01:04:55 -0500

    binary search algo continued

--- 275.h-index-ii.py
+++ 275.h-index-ii.py
@@ -5,8 +5,22 @@
 #
 
 # @lc code=start
+from typing import List
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        
+        left = 1
+        right = len(citations)
+        while left <= right:
+            mid = (left + right) // 2
+            # if citations[-mid] >= mid:
+            #     left = mid + 1
+            # else:
+            #     right = mid - 1
+
+            if citations[-mid] < mid:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return right
 # @lc code=end
 
