19ca8a02747d0142bf1df28eef11b3ec2ab18f97
JiajunLi <jl11895@nyu.edu>
2025-03-04 15:09:36 -0500

    Binary Search

---
+++ 410.split-array-largest-sum.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=410 lang=python3
+#
+# [410] Split Array Largest Sum
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        def check(maxSum: int) -> bool:
+            count = 1
+            sum = 0
+            for num in nums:
+                if sum + num > maxSum:
+                    if count == k:
+                        return False
+                    count += 1
+                    sum = num
+                else:
+                    sum += num
+            return True
+        right = sum(nums) 
+        left = max(nums) - 1
+        while left + 1 < right:
+            mid = (left + right) //2
+            if check(mid):
+                right = mid
+            else:
+                left = mid
+        return right
+# @lc code=end
+
