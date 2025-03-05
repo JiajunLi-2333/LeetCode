2c5f2fc1530a93d2d6f9ba8f3f34576cf05e79a3
JiajunLi <jl11895@nyu.edu>
2025-03-05 14:59:26 -0500

    LeetCoding

---
+++ 2513.minimize-the-maximum-of-two-arrays.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=2513 lang=python3
+#
+# [2513] Minimize the Maximum of Two Arrays
+#
+
+# @lc code=start
+class Solution:
+    def minimizeSet(self, divisor1: int, divisor2: int, uniqueCnt1: int, uniqueCnt2: int) -> int:
+        
+# @lc code=end
+
