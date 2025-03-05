d2fcf1b9a8cc1b3f33ebea665251ea7a8f426f03
JiajunLi <jl11895@nyu.edu>
2025-03-04 20:37:29 -0500

    call it a day

--- 1760.minimum-limit-of-balls-in-a-bag.py
+++ 1760.minimum-limit-of-balls-in-a-bag.py
@@ -8,7 +8,21 @@
 from typing import List
 class Solution:
     def minimumSize(self, nums: List[int], maxOperations: int) -> int:
-        
-        
+        def check(maxBalls: int) -> bool:
+            ops = 0
+            for num in nums:
+                if num > maxBalls:
+                    # -1 because I am counting number of operations to reduce the number of balls to maxBalls
+                    ops += (num + maxBalls - 1) // maxBalls - 1
+            return ops <= maxOperations
+        left = 0
+        right = max(nums)
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if check(mid):
+                right = mid
+            else:
+                left = mid
+        return right       
 # @lc code=end
 
