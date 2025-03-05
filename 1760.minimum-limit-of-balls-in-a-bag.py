16de604f184797d37d151a8c4c6b25674c3181d1
JiajunLi <jl11895@nyu.edu>
2025-03-04 19:39:43 -0500

    updating everything

---
+++ 1760.minimum-limit-of-balls-in-a-bag.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=1760 lang=python3
+#
+# [1760] Minimum Limit of Balls in a Bag
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
+        
+        
+# @lc code=end
+
