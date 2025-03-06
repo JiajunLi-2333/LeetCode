4785405961237679e8509621f31ec6fbc75f206b
JiajunLi <jl11895@nyu.edu>
2025-03-06 18:09:32 -0500

    review session needed

--- 2517.maximum-tastiness-of-candy-basket.py
+++ 2517.maximum-tastiness-of-candy-basket.py
@@ -11,7 +11,7 @@ class Solution:
         price.sort()
         # 1 2 5 8 13 21 
         #Check function is always a pain in the ass for binary search problems
-        def check(minDiff: int) -> bool:
+        def check(minDiff: int) -> int:
             cnt = 1
             i = j = 0
             n = len(price)
@@ -20,12 +20,12 @@ class Solution:
                     cnt += 1
                     j = i
                 i += 1
-            return cnt >= k             
+            return cnt             
         left = 0
         right = price[-1] - price[0] + 1
         while left + 1 < right:
             mid = (left + right) // 2
-            if check(mid):
+            if check(mid) >= k:
                 left = mid
             else:
                 right = mid
