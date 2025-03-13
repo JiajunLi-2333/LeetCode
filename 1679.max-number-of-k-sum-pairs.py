33b6de2e138074aff7728fe6b32b1b6b5f63c2cd
JiajunLi <jl11895@nyu.edu>
2025-03-13 00:12:16 -0400

    Finish 3371 on thinking process

--- 1679.max-number-of-k-sum-pairs.py
+++ 1679.max-number-of-k-sum-pairs.py
@@ -30,20 +30,51 @@ class Solution:
         # return ans
 
         #todo If I use two pointers
-        nums.sort()
-        n = len(nums)
-        left = 0
-        right = n - 1
-        ans = 0 
-        while left < right:
-            if nums[left] + nums[right] == k:
+        # nums.sort()
+        # n = len(nums)
+        # left = 0
+        # right = n - 1
+        # ans = 0 
+        # while left < right:
+        #     if nums[left] + nums[right] == k:
+        #         ans += 1
+        #         left += 1
+        #         right -= 1
+        #     elif nums[left] + nums[right] < k:
+        #         left += 1
+        #     else:
+        #         right -= 1
+        # return ans
+
+        #the question can be translated into 
+        #find the number of unique pairs that sum up to k
+
+        #two pointer
+        # nums.sort()
+        # left = ans = 0
+        # right = len(nums) -1
+        # while left < right:
+        #     if nums[left] + nums[right] > k:
+        #         right -= 1
+        #     elif nums[left] + nums[right] < k:
+        #         left += 1
+        #     else:
+        #         ans += 1
+        #         left += 1 
+        #         right -=  1
+        # return ans
+
+        #Iterating right and maintain left
+        #Slower tha two pointer 
+        ans = 0
+        hashmap = Counter()
+        for num in nums:
+            rest = k - num
+            if hashmap[rest] > 0:
                 ans += 1
-                left += 1
-                right -= 1
-            elif nums[left] + nums[right] < k:
-                left += 1
+                hashmap[rest] -= 1
             else:
-                right -= 1
+                hashmap[num] += 1
         return ans
 # @lc code=end
 
---
+++ 2784.check-if-array-is-good.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=2784 lang=python3
+#
+# [2784] Check if Array is Good
+#
+
+# @lc code=start
+class Solution:
+    def isGood(self, nums: List[int]) -> bool:
+        
+# @lc code=end
+
