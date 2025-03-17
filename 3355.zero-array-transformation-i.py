8b9d054e72dc12146abe125f352fa52c6ce3908c
JiajunLi <jl11895@nyu.edu>
2025-03-17 00:17:01 -0400

    taking a break

---
+++ 3355.zero-array-transformation-i.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=3355 lang=python3
+#
+# [3355] Zero Array Transformation I
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        
+        
+# @lc code=end
+
