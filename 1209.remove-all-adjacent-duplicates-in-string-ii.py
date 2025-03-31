05d31542631377e41fd1c7b02510c0193a4ac932
JiajunLi <jl11895@nyu.edu>
2025-03-31 16:43:09 -0400

    Keep on the data structure queue

--- 1209.remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209.remove-all-adjacent-duplicates-in-string-ii.py
@@ -17,7 +17,7 @@ class Solution:
             if stack and stack[-1][0] == c:
                 stack[-1][1] += 1
                 if stack[-1][1] == k:
-                    stack.pop()
+                    stack.pop()ø
             else:
                 stack.append([c,1])
         return ''.join([c * k for c,k in stack])
