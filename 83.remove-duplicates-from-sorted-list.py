022b59fab1926f74dffc158009c563b1b4c859d0
JiajunLi <jl11895@nyu.edu>
2025-04-07 15:50:13 -0400

    linked list is somewhat tricky

---
+++ 83.remove-duplicates-from-sorted-list.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=83 lang=python3
+#
+# [83] Remove Duplicates from Sorted List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # cur = head
+        # while cur:
+        #     while cur.next and cur.val == cur.next.val:
+        #         cur.next = cur.next.next
+        #     cur = cur.next
+        # return head
+        #optimize
+        if not head:
+            return head
+        cur = head
+        while cur.next:
+            if cur.val == cur.next.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return head
+            
+
+        
+# @lc code=end
+
