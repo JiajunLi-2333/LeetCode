49c3e8c5f36557e8c02b2ef13fe4e4c50e605c2d
JiajunLi <jl11895@nyu.edu>
2025-04-09 15:30:28 -0400

    Linked List

--- 2487.remove-nodes-from-linked-list.py
+++ 2487.remove-nodes-from-linked-list.py
@@ -12,7 +12,36 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
-    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-# @lc code=end
+    #todo reverse the linked list and use iterative method to remove the nodes
+    # def reverseNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     prev = None
+    #     cur = head
+    #     while cur:
+    #         next_node = cur.next
+    #         cur.next = prev
+    #         prev = cur
+    #         cur = next_node
+    #     return prev
 
+
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # head = self.reverseNodes(head)
+        # cur = head
+        # while cur and cur.next:
+        #     if cur.val > cur.next.val:
+        #         cur.next = cur.next.next
+        #     else:
+        #         cur = cur.next
+        # return self.reverseNodes(head)
+
+        #todo use recursive method to remove the nodes
+        if head.next is None:
+            return head
+        node = self.removeNodes(head.next)
+        if node.val > head.val:
+            return node
+        head.next = node
+        return head 
+      
+    
+# @lc code=end
---
+++ 2807.insert-greatest-common-divisors-in-linked-list.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=2807 lang=python3
+#
+# [2807] Insert Greatest Common Divisors in Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+# @lc code=end
+
