a9cd53f9c1821307de89fb8a3fc2d4ebc994b054
JiajunLi <jl11895@nyu.edu>
2025-05-28 15:13:50 -0400

    Done for now

--- 219.contains-duplicate-ii.java
+++ 219.contains-duplicate-ii.java
@@ -5,9 +5,25 @@
  */
 
 // @lc code=start
+import java.util.HashMap;
+import java.util.Map;
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        
+        Map<Integer, Integer> map = new HashMap<>();
+        int minDis = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length;i++){
+            if(map.containsKey(nums[i])){
+                minDis = Math.min(minDis, i - map.get(nums[i]));
+                map.put(nums[i], i);
+            }
+            else{
+                map.put(nums[i], i);
+            }
+            if (minDis <= k){
+                return true;
+            }
+        }
+        return false; 
     }
 }
 // @lc code=end
---
+++ 2260.minimum-consecutive-cards-to-pick-up.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=2260 lang=java
+ *
+ * [2260] Minimum Consecutive Cards to Pick Up
+ */
+
+// @lc code=start
+class Solution {
+    public int minimumCardPickup(int[] cards) {
+        
+    }
+}
+// @lc code=end
+
