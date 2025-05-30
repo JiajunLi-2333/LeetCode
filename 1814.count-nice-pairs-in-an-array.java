4338b1e5406dfa7a242771867d56abd6ee23ae5a
JiajunLi <jl11895@nyu.edu>
2025-05-30 19:15:24 -0400

    Continuing

--- 1814.count-nice-pairs-in-an-array.java
+++ 1814.count-nice-pairs-in-an-array.java
@@ -5,9 +5,33 @@
  */
 
 // @lc code=start
+import java.util.HashMap;
+import java.util.Map;
 class Solution {
     public int countNicePairs(int[] nums) {
-        
+        //i < j
+        //nums[i] + rev(nums[j]) == nums[j] + rev(nums[i])
+        //nums[i] - rev(nums[i]) == nums[j] - rev(nums[j]) Equivalent solves the problem
+        double MOD = Math.pow(10, 9) + 7;
+        int n = nums.length; 
+        int ans = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            int diff = nums[i] - getReverse(nums[i]);
+            ans += map.getOrDefault(diff, 0);
+            ans %= MOD;
+            map.put(diff, map.getOrDefault(diff, 0) + 1);
+        }
+        return ans;
+
+    }
+    private int getReverse(int num){
+        int rev = 0;
+        while (num > 0){
+            rev = 10 * rev + num % 10;
+            num /= 10;
+        }
+        return rev;
     }
 }
 // @lc code=end
---
+++ 2905.find-indices-with-index-and-value-difference-ii.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=2905 lang=java
+ *
+ * [2905] Find Indices With Index and Value Difference II
+ */
+
+// @lc code=start
+class Solution {
+    public int[] findIndices(int[] nums, int indexDifference, int valueDifference) {
+        
+    }
+}
+// @lc code=end
+
