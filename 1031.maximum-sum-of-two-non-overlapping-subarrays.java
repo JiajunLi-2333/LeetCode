e42edf87c7c535d8783b3ffddbb18b25227b820a
JiajunLi <jl11895@nyu.edu>
2025-05-31 13:23:17 -0400

    Getting hard

---
+++ 1031.maximum-sum-of-two-non-overlapping-subarrays.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1031 lang=java
+ *
+ * [1031] Maximum Sum of Two Non-Overlapping Subarrays
+ */
+
+// @lc code=start
+class Solution {
+    public int maxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen) {
+        
+    }
+}
+// @lc code=end
+
--- 2874.maximum-value-of-an-ordered-triplet-ii.java
+++ 2874.maximum-value-of-an-ordered-triplet-ii.java
@@ -7,7 +7,15 @@
 // @lc code=start
 class Solution {
     public long maximumTripletValue(int[] nums) {
-        
+        long ans = 0; 
+        int maxDiff = 0;
+        int max = 0;
+        for (int num : nums){
+            ans  = Math.max(ans, (long)maxDiff * num);
+            maxDiff = Math.max(maxDiff, max - num);
+            max = Math.max(max, num);
+        }
+        return ans;
     }
 }
 // @lc code=end
