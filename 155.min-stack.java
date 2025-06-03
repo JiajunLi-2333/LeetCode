11a8c9c482e771614c1361ab879e5ad86e624e77
JiajunLi <jl11895@nyu.edu>
2025-06-03 18:34:13 -0400

    Need to reinforce the concept of prefix

--- 155.min-stack.java
+++ 155.min-stack.java
@@ -3,28 +3,29 @@
  *
  * [155] Min Stack
  */
-
+import java.util.Stack;
 // @lc code=start
 class MinStack {
-
+    private Stack<int[]> stack = new Stack<>();
     public MinStack() {
         
     }
     
     public void push(int val) {
-        
+        int min = stack.isEmpty() ? val : Math.min(val, getMin());
+        stack.push(new int[]{val,min});
     }
     
     public void pop() {
-        
+        stack.pop();
     }
     
     public int top() {
-        
+        return stack.peek()[0];
     }
     
     public int getMin() {
-        
+        return stack.peek()[1];
     }
 }
 
