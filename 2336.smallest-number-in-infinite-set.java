17d49498948e397d5c452c60f9ec1ca2b638d835
JiajunLi <jl11895@nyu.edu>
2025-07-23 12:27:09 -0400

    same as last commiy

--- 2336.smallest-number-in-infinite-set.java
+++ 2336.smallest-number-in-infinite-set.java
@@ -3,23 +3,34 @@
  *
  * [2336] Smallest Number in Infinite Set
  */
+import java.util.Arrays;
 import java.util.PriorityQueue;
 // @lc code=start
 class SmallestInfiniteSet {
-    private final int[] map = new int[1001];
+    private final int[] map = new int[1010];
     private final PriorityQueue<Integer> pq;
 
     public SmallestInfiniteSet() {
         pq = new PriorityQueue<>();
+        Arrays.fill(map, 0);//0 means the number is not used 
+        for(int i = 1; i <= 1000; i++){
+            pq.offer(i);
+            map[i] = 1; //Mark as used here
+        }
     }
     
     public int popSmallest() {
-        int cur =
+        int cur = pq.poll();
+        map[cur] = 0; //mark as unused
+        return cur;
         
     }
     
     public void addBack(int num) {
-        
+        if(map[num] == 0){
+            pq.offer(num);
+            map[num] = 1; //mark as used
+        }
     }
 }
 
