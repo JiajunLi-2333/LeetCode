75ad3d5dda1de9e935426e3724a05e43d6827748
JiajunLi <jl11895@nyu.edu>
2025-08-27 21:21:35 -0400

    Keep Grinding

--- 1888.minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 1888.minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -6,34 +6,49 @@
 
 // @lc code=start
 class Solution {
-    public int minFlips(String s) {
-        String s2 = s + s;
-        int One_pattern = 0;
-        int Zero_pattern = 0;
-        int ans = Integer.MAX_VALUE;
-        for(int i = 0; i < s2.length(); i++){
-            if(i % 2 == 0){// Even position
-                if (s2.charAt(i) == '0') One_pattern++;
-                else Zero_pattern++;
-            }
-            else{
-                if(s2.charAt(i) == '0') Zero_pattern++;
-                else One_pattern++;
-            }
+    public int minFlips(String S) {
+        // String s2 = s + s;
+        // int One_pattern = 0;
+        // int Zero_pattern = 0;
+        // int ans = Integer.MAX_VALUE;
+        // for(int i = 0; i < s2.length(); i++){
+        //     if(i % 2 == 0){// Even position
+        //         if (s2.charAt(i) == '0') One_pattern++;
+        //         else Zero_pattern++;
+        //     }
+        //     else{
+        //         if(s2.charAt(i) == '0') Zero_pattern++;
+        //         else One_pattern++;
+        //     }
 
-            if(i < s.length() -1) continue;
-            ans = Math.min(ans, Math.min(One_pattern, Zero_pattern));
-            int j = i - s.length() + 1;
-            if(j % 2 == 0){
-                if (s2.charAt(i - s.length() + 1) == '0') One_pattern--;
-                else Zero_pattern--;
-            }
-            else{
-                if(s2.charAt(i - s.length() + 1) == '0') Zero_pattern--;
-                else One_pattern--;
-            }
+        //     if(i < s.length() -1) continue;
+        //     ans = Math.min(ans, Math.min(One_pattern, Zero_pattern));
+        //     int j = i - s.length() + 1;
+        //     if(j % 2 == 0){
+        //         if (s2.charAt(i - s.length() + 1) == '0') One_pattern--;
+        //         else Zero_pattern--;
+        //     }
+        //     else{
+        //         if(s2.charAt(i - s.length() + 1) == '0') Zero_pattern--;
+        //         else One_pattern--;
+        //     }
+        // }
+        // return ans; 
+
+        //! solid thinking and systematic knowledgebase are the key to leetcode
+        
+        char[] s = S.toCharArray();
+        int n = s.length;
+        int ans = n;
+        int cnt = 0;
+        for(int i = 0; i < 2*n - 1; i++){
+            if(s[i % n] % 2 != i % 2) cnt++;
+            int left = i - n + 1;
+            if(left < 0) continue;
+            ans = Math.min(ans, Math.min(cnt, n - cnt));
+            if(s[left] % 2 != left % 2) cnt--;
         }
-        return ans; 
+        return ans;
     }
 }
 // @lc code=end
--- 2653.sliding-subarray-beauty.java
+++ 2653.sliding-subarray-beauty.java
@@ -3,37 +3,89 @@
  *
  * [2653] Sliding Subarray Beauty
  */
-
+import java.util.*;
 // @lc code=start
 class Solution {
+    PriorityQueue<Integer> minpq;
+    PriorityQueue<Integer> maxpq;
+    int[] location;
+    int maxpqHave;
+    int minpqHave;
     public int[] getSubarrayBeauty(int[] nums, int k, int x) {
-        //use counting sort to count the number of elements in the current window
-        int[] counter = new int[50];
-        int[] result = new int[nums.length - k + 1];
-        
-        for(int i = 0; i < k -1; i++){
-            if(nums[i] < 0){//care only about the negative numbers
-                counter[nums[i] + 50]++;
-            }
-        }
-        for(int i = k -1; i < nums.length; i++){
-            if(nums[i] < 0){
-                counter[nums[i]+50]++;
-            }
-            int count = x;
-            for(int j = 0; j < counter.length; j++){
-                count -= counter[j];
-                if(count <= 0){
-                    result[i - k + 1] = j - 50;
-                    break;
-                }
-            }
-            if(nums[i - k + 1] < 0){
-                counter[nums[i - k + 1] + 50]--;
-            }
-        }
-        return result;
+        minpq = new PriorityQueue<>(Comparator.comparingInt(a -> nums[a]));
+        maxpq = new PriorityQueue<>((a,b)-> nums[b] - nums[a]);
+        location = new int[nums.length];// 0为删除 1在大堆 -1在小堆
+        maxpqHave = 0; // 大堆有效元素
+        minpqHave = 0; // 小堆有效元素
 
+        int[] ans = new int[nums.length - k + 1];
+        for (int i = 0; i < nums.length; i++) {
+            // 入窗口
+            if (nums[i] < 0){
+                maxpq.add(i);// 不管什么元素，只要符合条件(<0)先入大顶堆
+                location[i] = 1;
+                maxpqHave++;
+            }
+            if (i >= k && nums[i - k] < 0){
+                //延迟删除
+                if (location[i - k] < 0){
+                    minpqHave--;
+                }else {
+                    maxpqHave--;
+                }
+                location[i - k] = 0; // 标记删除
+            }
+            if (i >= k - 1){
+                ans[i - k + 1] = pqGet(x, nums);
+            }
+        }
+        return ans;
+    }
+
+    private int pqGet(int x, int[] nums) {
+        pqAdjust(nums);
+        while (maxpqHave > x){
+            pqRemove(maxpq);
+            Integer poll = maxpq.poll();
+            minpq.add(poll);
+            maxpqHave--;
+            minpqHave++;
+            location[poll] = -1;
+        }
+        while (maxpqHave < x){
+            pqRemove(minpq);
+            if (minpqHave > 0){
+                Integer poll = minpq.poll();
+                maxpq.add(poll);
+                maxpqHave++;
+                minpqHave--;
+                location[poll] = 1;
+            }else if (maxpqHave < x){
+                break;
+            }
+        }
+        pqRemove(maxpq);
+        if (maxpqHave == x){
+            return nums[maxpq.peek()];
+        }
+        return 0;
+    }
+    private void pqAdjust(int[] nums) {
+        while (maxpqHave > 0 && minpqHave > 0 && nums[maxpq.peek()] > nums[minpq.peek()]){
+            pqRemove(maxpq);
+            pqRemove(minpq);
+            Integer poll = maxpq.poll();
+            minpq.add(poll);
+            location[poll] = -1;
+            maxpqHave--;
+            minpqHave++;
+        }
+    }
+
+    private void pqRemove(PriorityQueue<Integer> pq) {
+        while (!pq.isEmpty() && location[pq.peek()] == 0){
+            pq.poll();
+        }
     }
 }
 // @lc code=end
