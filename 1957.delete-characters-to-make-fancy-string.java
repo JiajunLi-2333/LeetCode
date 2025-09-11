0a0d38e34486e9ce8d2aed1f94f4b7c82c5813f7
JiajunLi <jl11895@nyu.edu>
2025-09-11 10:13:07 -0400

    Nothing

--- 1957.delete-characters-to-make-fancy-string.java
+++ 1957.delete-characters-to-make-fancy-string.java
@@ -3,10 +3,31 @@
  *
  * [1957] Delete Characters to Make Fancy String
  */
-
+import java.util.*;
 // @lc code=start
 class Solution {
     public String makeFancyString(String s) {
+        //! Approach 1: stack: Slow but works
+        // Deque<Character> stack = new ArrayDeque<>();
+        // int cnt = 0;
+        // for(char c : s.toCharArray()){
+        //     if(!stack.isEmpty() && stack.peek() == c){
+        //         cnt++;
+        //         if(cnt < 2){
+        //             stack.push(c);
+        //         }
+        //     }
+        //     else{
+        //         cnt = 0;
+        //         stack.push(c);
+        //     }
+        // }
+        // StringBuilder sb = new StringBuilder();
+        // while(!stack.isEmpty()){
+        //     sb.append(stack.pollLast());
+        // }
+        // return sb.toString();
+        //! Approach 2: Use Two pointers
         
     }
 }
