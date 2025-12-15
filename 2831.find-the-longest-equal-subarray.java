74fdd2fa87d5b30bcd8914afe8ada01d3fcf07d6
JiajunLi <jl11895@nyu.edu>
2025-12-15 13:04:26 -0500

    update the repo

---
+++ 2831.find-the-longest-equal-subarray.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=2831 lang=java
+ *
+ * [2831] Find the Longest Equal Subarray
+ */
+import java.util.*;
+// @lc code=start
+class Solution {
+    public int longestEqualSubarray(List<Integer> nums, int k) {
+        
+    }
+}
+// @lc code=end
+
