4232785c788ee4191d49540e9d19aaef46e3d505
JiajunLi <jl11895@nyu.edu>
2026-02-25 21:59:43 -0500

    Doing my best

--- 2364.count-number-of-bad-pairs.java
+++ 2364.count-number-of-bad-pairs.java
@@ -8,8 +8,31 @@ import java.util.Map;
 // @lc code=start
 class Solution {
     public long countBadPairs(int[] nums) {
-       
+       //clarification
+       //count the number of bad paris, where a pair is
+       // (i < j) && j - i != nums[j] - nums[i]
 
+       //constraints
+       //1 <= nums.length <= 10 ^ 5
+       //1 <= nums[i] <= 10 ^ 9
+
+       //edge case
+       // nums = [1] -> 0
+       // nums = [3,3,3,3,3] -> all pairs are bad paris
+       // ans could be quite long
+
+       //solution
+       // nums[i] - i != nums[j] - j
+
+       long ans = 0;
+       Map<Integer, Integer> cnt = new HashMap<>();
+       for(int j = 0; j < nums.length; j++){
+            int val = nums[j] - j;
+            int points = j - cnt.getOrDefault(val, 0);
+            ans += points;
+            cnt.put(val, cnt.getOrDefault(val, 0) + 1);
+       }
+       return ans;
     }
 }
 // @lc code=end
--- 3371.identify-the-largest-outlier-in-an-array.java
+++ 3371.identify-the-largest-outlier-in-an-array.java
@@ -9,25 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 class Solution {
     public int getLargestOutlier(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int sum = 0, ans = Integer.MIN_VALUE;
-        for (int num : nums){
-            map.merge(num, 1, Integer :: sum );
-            sum += num;
-        } 
-
-        for (int num : nums){
-            int outlier = sum - 2 * num;
-            // If the outlier is equal to the current number and it appears only once, we can skip it
-            if (outlier == num && map.get(num) == 1) {
-                continue; 
-            }
-            if(map.containsKey(outlier) && map.get(outlier) > 0){
-                ans = Math.max(ans, outlier);
-            }
-
-        }
-        return ans;
+        
 
     }
 }
