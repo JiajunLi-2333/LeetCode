45f12a88841d9d569b3fce8bdb6ad1cbde5cab87
JiajunLi <jl11895@nyu.edu>
2026-03-02 16:12:32 -0500

    ...

--- 1372.longest-zig-zag-path-in-a-binary-tree.java
+++ 1372.longest-zig-zag-path-in-a-binary-tree.java
@@ -21,8 +21,40 @@
  * }
  */
 class Solution {
+    // private int ans = 0; 
+    //TODO 
+    private int ans = 0; 
     public int longestZigZag(TreeNode root) {
+        // dfs(root, true, 0);
+        // return ans;
+        dfs(root);
+        return ans;
     }
+    // private void dfs(TreeNode root, Boolean direct, int len){
+    //     if(root == null) return;
+    //     ans = Math.max(ans, len);
+    //     if(direct){//true means this node is the left child of its parent node
+    //         dfs(root.left, true, 1);
+    //         dfs(root.right, false, len + 1); 
+    //     }
+    //     else{
+    //         dfs(root.left, true, len + 1);
+    //         dfs(root.right, false, 1);
+    //     }
+    // }
+
+    //TODO dfs from bottom to top
+    private int[] dfs(TreeNode root){
+        if(root == null) return new int[]{-1,-1};
+        int[] left = dfs(root.left);
+        int[] right = dfs(root.right);
+
+        int leftLen = left[1] + 1;
+        int rightLen = right[0] + 1;
+        ans = Math.max(ans, Math.max(leftLen, rightLen));
+        return new int[]{leftLen, rightLen};
+    }
+
     
 }
 // @lc code=end
