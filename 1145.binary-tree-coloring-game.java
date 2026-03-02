20b0a24c05abdfeff745f4279c4cf413060fd441
JiajunLi <jl11895@nyu.edu>
2026-03-02 16:18:03 -0500

    stop for now

---
+++ 1145.binary-tree-coloring-game.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1145 lang=java
+ *
+ * [1145] Binary Tree Coloring Game
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
+        
+    }
+}
+// @lc code=end
+
