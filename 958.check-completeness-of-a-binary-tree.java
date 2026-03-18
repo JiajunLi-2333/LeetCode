6d3f6172807a67054e2cdd0f489c1931fad44aae
JiajunLi <jl11895@nyu.edu>
2026-03-18 18:27:18 -0400

    Binary Tree

--- 958.check-completeness-of-a-binary-tree.java
+++ 958.check-completeness-of-a-binary-tree.java
@@ -20,9 +20,23 @@
  *     }
  * }
  */
+import java.util.*;
 class Solution {
     public boolean isCompleteTree(TreeNode root) {
-        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean seenNull = false;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) {
+                seenNull = true;
+            } else {
+                if (seenNull) return false;
+                queue.offer(node.left);   // null 也入队
+                queue.offer(node.right);  // null 也入队
+            }
+        }
+        return true;
     }
 }
 // @lc code=end
