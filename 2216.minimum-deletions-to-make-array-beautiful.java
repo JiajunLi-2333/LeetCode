3cf098115eaf20bb9daa2004bdba13c73a51acaf
JiajunLi <jl11895@nyu.edu>
2026-03-20 15:11:24 -0400

    Stack

--- 2216.minimum-deletions-to-make-array-beautiful.java
+++ 2216.minimum-deletions-to-make-array-beautiful.java
@@ -3,31 +3,38 @@
  *
  * [2216] Minimum Deletions to Make Array Beautiful
  */
-import java.util.Stack;
+import java.util.*;
 // @lc code=start
 class Solution {
+    // public int minDeletion(int[] nums) {
+    //     Deque<Integer> stack = new ArrayDeque<>();
+    //     int ans = 0;
+    //     for(int num : nums){
+    //         if(!stack.isEmpty() && stack.size() % 2 == 1 && stack.peek() == num){
+    //             ans++;
+    //         }else{
+    //             stack.push(num); 
+    //         }
+    //     }
+    //     if(stack.size() %2 ==1){
+    //         ans++;
+    //     }
+    //     return ans;  
+    // }
+
+    //?栈思想上的优化方案
     public int minDeletion(int[] nums) {
-        int size = 0, ans = 0;
-        Stack<Integer> stack = new Stack<>();
-        for(int num : nums){
-            int curIndex = size - 1;
-            if(size > 0 && curIndex % 2 == 0 && stack.peek() == num){
-                size+=2;
-                stack.push(num);
-                ans++;
-            }
-            else{
-                stack.push(num);
-                size++;
-            }
+    int ans = 0, size = 0;
+    for (int i = 0; i < nums.length; i++) {
+        if (size % 2 == 0 && i + 1 < nums.length && nums[i] == nums[i + 1]) {
+            ans++; // 删掉当前元素
+        } else {
+            size++; // 保留当前元素
         }
-        //I forget I need it to be even length
-        if (size % 2 == 1){
-            ans++;
-        }
-        return ans;
-        
     }
+    if (size % 2 == 1) ans++;
+    return ans;
+}
 }
 // @lc code=end
 
