a0d50844b0012fd286ce15f8dfaea3c70b74195f
JiajunLi <jl11895@nyu.edu>
2026-03-22 03:15:16 -0400

    Read carefully the question and convert the brutal thought into code

--- 856.score-of-parentheses.java
+++ 856.score-of-parentheses.java
@@ -8,7 +8,7 @@ Clarify:
 The string s is already balanced -> every ( has a ). "()" has one point, AB = A + B. (A) = 2 * A. 
 ?:
 Example (()((())))() = 2*(1 + 2*(2 * 1)) + 1 = 11
-Brutal: Let us abstract s = A + B + C where A, B, C are all balanced parentheses strings that constitutes strings. we can identify each one of them by # of ( == # of ). And we can recursively calculate A, B, C, add them up and spawn the answer. So this can be solved by Recursion. TC O(n^2);
+Brutal: Let us abstract s = A + B + C where A, B, C are all balanced parentheses strings that constitutes string s. we can identify each one of them by # of ( == # of ). And we can recursively calculate A, B, C, add them up and spawn the answer. So this can be solved by Recursion. TC O(n^2);
 Bottleneck:
 We are scaning the string over and over here because we do not know which layer we are at right now so we do not know whether we need to do addition or multiplication.And it would be so much better if we could know which layer we are at right now
 Keyword: Nested tracking -> stack
