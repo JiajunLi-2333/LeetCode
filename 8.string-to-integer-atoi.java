a48e2545b3648e5c5a3fccb186175efa424d5ff0
JiajunLi <jl11895@nyu.edu>
2026-03-22 22:56:15 -0400

    stack and expression

--- 8.string-to-integer-atoi.java
+++ 8.string-to-integer-atoi.java
@@ -7,7 +7,29 @@
 // @lc code=start
 class Solution {
     public int myAtoi(String s) {
-        
+        s = s.trim();
+        if (s.isEmpty()) return 0;
+
+        int sign = 1;
+        int res = 0;
+        int i = 0;
+        if (s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        } else if (s.charAt(i) == '+') {
+            i++;
+        }
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+            if (res > (Integer.MAX_VALUE - digit) / 10) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            res = res * 10 + digit;
+            i++;
+        }
+
+        return sign * res;
     }
 }
 // @lc code=end
