4550eeeb83dd866ec034b598c1675f2949413937
JiajunLi-2333 <jl11895@nyu.edu>
2026-03-28 00:01:30 -0400

    Stack and algo thinking

--- 1944.number-of-visible-people-in-a-queue.java
+++ 1944.number-of-visible-people-in-a-queue.java
@@ -7,8 +7,19 @@ import java.util.*;
 // @lc code=start
 class Solution {
     public int[] canSeePersonsCount(int[] heights) {
-        
-        
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] ans = new int[heights.length];
+        Arrays.fill(ans, 0);
+        for(int i = 0; i < heights.length; i++){
+            int height = heights[i];
+            while(!stack.isEmpty() && height > heights[stack.peek()] ){
+                int index = stack.pop();
+                ans[index]++; 
+            }
+            if(!stack.isEmpty()) ans[stack.peek()]++;
+            stack.push(i);
+        }
+        return ans;     
     }
 }
 // @lc code=end
