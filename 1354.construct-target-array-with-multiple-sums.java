27c4fa843b393f7e0ded28a2abceb8a7b74177bb
JiajunLi-2333 <jl11895@nyu.edu>
2026-04-06 14:12:02 -0400

    looking through the problem

--- 1354.construct-target-array-with-multiple-sums.java
+++ 1354.construct-target-array-with-multiple-sums.java
@@ -3,32 +3,11 @@
  *
  * [1354] Construct Target Array With Multiple Sums
  */
-import java.util.PriorityQueue;
+import java.util.*;
 // @lc code=start
 class Solution {
     public boolean isPossible(int[] target) {
-        //reverse the array and use priority queue to simulate the process
-        PriorityQueue<Long> pq = new PriorityQueue<>((a, b) -> Long.compare(b, a));
-        long sum = 0;
-        for(int num : target){
-            pq.offer(num * 1L);
-            sum += num;
-        }
-        while(!pq.isEmpty() && pq.peek() > 1){
-            Long cur = pq.poll();
-            sum -= cur;
-            if(sum <= 0 || sum >= cur) return false;
-            //calculate the new value to be added back to the queue
-
-
-            //This solves the problem of newVal is still or not the largest value in the queue 
-            Long newVal = cur % sum;
-            if(newVal == 0) newVal = sum; //if cur is a multiple
-            pq.offer(newVal);
-            sum += newVal; //update the sum with the new value
-
-        }
-        return true;
+        
     }
 }
 // @lc code=end
--- 1882.process-tasks-using-servers.java
+++ 1882.process-tasks-using-servers.java
@@ -7,12 +7,39 @@ import java.util.*;
 // @lc code=start
 class Solution {
     public int[] assignTasks(int[] servers, int[] tasks) {
-       
-    }   
+        int n = servers.length, m = tasks.length;
+        
+        //? available minHeap (weight. index)
+        PriorityQueue<int[]> available = new PriorityQueue<>(
+            (a,b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]
+        );
+
+        //? unavailable minHeap (freeTime, weight, index)
+        PriorityQueue<int[]> unavailable = new PriorityQueue<>(
+            (a,b) -> a[0] - b[0]
+        );
+
+        for(int i = 0; i < n; i++){
+            available.offer(new int[]{servers[i], i});
+        }
+
+        int[] ans = new int[m];
+        int t = 0;
+        for(int j = 0; j < m ;j++){
+            t = Math.max(t, j);
+            if(available.isEmpty()){
+                t = Math.max(t, unavailable.peek()[0]);
+            }
+            while(!unavailable.isEmpty() && t >= unavailable.peek()[0]){
+                int[] s = unavailable.poll();
+                available.offer(new int[]{s[1], s[2]});
+            }
+
+            int[] assign = available.poll();
+            ans[j] = assign[1];
+            unavailable.offer(new int[]{t + tasks[j], assign[0], assign[1]});
+        }
+        return ans;
+    }
 }
-
-
-
-
-
 // @lc code=end
