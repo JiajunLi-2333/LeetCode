230c7cca8eba5744b87aed68e3639c21b81bb9e7
JiajunLi <jl11895@nyu.edu>
2026-04-23 21:37:39 -0400

    BST

---
+++ 530.minimum-absolute-difference-in-bst.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=530 lang=java
+ *
+ * [530] Minimum Absolute Difference in BST
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int ans = Integer.MAX_VALUE;
+    private Integer prev = null;
+
+    public int getMinimumDifference(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+    private void dfs(TreeNode root){
+        if(root == null) return;
+        dfs(root.left);
+        if(prev != null) ans = Math.min(ans, root.val - prev);
+        prev = root.val;
+        dfs(root.right);
+    }
+}
+// @lc code=end
+
