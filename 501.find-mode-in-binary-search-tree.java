eb0f8561941986896b8c3ac21d3a4647fc7bab30
JiajunLi-2333 <jl11895@nyu.edu>
2026-05-04 20:50:53 -0400

    Solved using the property of  BFS

--- 501.find-mode-in-binary-search-tree.java
+++ 501.find-mode-in-binary-search-tree.java
@@ -22,19 +22,27 @@
  */
 import java.util.*;
 class Solution {
-    private Map<Integer, Integer> map = new HashMap<>();
+    // private Map<Integer, Integer> map = new HashMap<>();
+    List<Integer> list = new ArrayList<>();
+    int max = 0, prev = Integer.MIN_VALUE,count = 0;
     public int[] findMode(TreeNode root) {
+        // dfs(root);
+        // int max = 0;
+        // for(Integer key: map.keySet()){
+        //     max = Math.max(max, map.get(key));
+        // }  
+        // List<Integer> list = new ArrayList<>();
+        // for(Integer key: map.keySet()){
+        //     if(map.get(key) == max){
+        //         list.add(key);
+        //     }
+        // }
+        // int[] res = new int[list.size()];
+        // for(int i = 0; i < list.size();i++){
+        //     res[i] = list.get(i);
+        // }
+        // return res;
         dfs(root);
-        int max = 0;
-        for(Integer key: map.keySet()){
-            max = Math.max(max, map.get(key));
-        }  
-        List<Integer> list = new ArrayList<>();
-        for(Integer key: map.keySet()){
-            if(map.get(key) == max){
-                list.add(key);
-            }
-        }
         int[] res = new int[list.size()];
         for(int i = 0; i < list.size();i++){
             res[i] = list.get(i);
@@ -43,15 +51,44 @@ class Solution {
     }
     public void dfs(TreeNode root){
         if(root == null) return;
-        if(map.containsKey(root.val)){
-            map.merge(root.val, 1,Integer::sum);
+        dfs(root.left);
+        if(prev == Integer.MIN_VALUE){
+            prev = root.val;
+            max = 1;
+            count = 1;
+            list.add(root.val); 
+        }
+        else if(prev == root.val){
+            count++;
+            if(count > max){
+                max = count;
+                list.clear();
+                list.add(root.val);
+            }
+            else if(count == max){
+                list.add(root.val);
+            }   
         }
         else{
-            map.put(root.val, 1);
+            count = 1;
+            if(count == max){
+                list.add(root.val);
+            }
         }
-        dfs(root.left);
+        prev = root.val;
         dfs(root.right);
-    }
+    } 
+    // public void dfs(TreeNode root){
+    //     if(root == null) return;
+    //     if(map.containsKey(root.val)){
+    //         map.merge(root.val, 1,Integer::sum);
+    //     }
+    //     else{
+    //         map.put(root.val, 1);
+    //     }
+    //     dfs(root.left);
+    //     dfs(root.right);
+    // }
 }
 // @lc code=end
 
